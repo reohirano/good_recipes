4f6040646115783b7317721515d9ee7e4a99d4e9
reohirano <137034337+reohirano@users.noreply.github.com>
2023-08-06 14:46:26 +0900

    テーブル、カラム作成

--- db/migrate/20230806053653_create_recipes.rb
+++ db/migrate/20230806053653_create_recipes.rb
@@ -1,7 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-
+      t.string :name
+      t.text :explain
+      t.integer :category_id
       t.timestamps
     end
   end
